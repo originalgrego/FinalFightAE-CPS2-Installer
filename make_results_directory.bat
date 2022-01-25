c4a349969bc0dca4fa8e12b081b8bff44633a542
Grego Lewandowski <original_grego@yahoo.com>
2022-01-25 11:10:13 -0500

    Add ips patches/data necessary for builds.

---
+++ make_results_directory.bat
@@ -0,0 +1,3 @@
+mkdir mister
+mkdir darksoft
+mkdir mame
--- make_work_directory.bat
+++ make_work_directory.bat
@@ -4,5 +4,6 @@ mkdir build1234abcd\combined\cps2_gfx
 mkdir build1234abcd\combined_patched
 mkdir build1234abcd\mister
 mkdir build1234abcd\mister\gfx
+mkdir build1234abcd\mister\snd
 mkdir build1234abcd\darksoft
 mkdir build1234abcd\mame
